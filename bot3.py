8b9bd3e91d979946e104d012d9f6f998022b4938
porygon-tech <59631625+porygon-tech@users.noreply.github.com>
2021-09-05 16:02:09 +0200

    Update bot3.py

--- bot3.py
+++ bot3.py
@@ -1,10 +1,12 @@
+#mineBot functions
+#author: miguel roman @porygon-tech
 import ctypes
 from ctypes import windll
 user32 = windll.user32
 user32.SetProcessDPIAware() 
 
 from time import sleep
-import numpy as np
+from numpy.random import normal as rnorm
 
 SendInput = ctypes.windll.user32.SendInput
 
@@ -18,12 +20,12 @@ class KeyBdInput(ctypes.Structure):
                 ("dwFlags", ctypes.c_ulong),
                 ("time", ctypes.c_ulong),
                 ("dwExtraInfo", PUL)]
-
+'''
 class HardwareInput(ctypes.Structure):
     _fields_ = [("uMsg", ctypes.c_ulong),
                 ("wParamL", ctypes.c_short),
                 ("wParamH", ctypes.c_ushort)]
-
+'''
 #https://docs.microsoft.com/en-us/windows/win32/api/winuser/ns-winuser-mouseinput
 class MouseInput(ctypes.Structure):
     _fields_ = [("dx", ctypes.c_long),
@@ -42,9 +44,8 @@ class Input(ctypes.Structure):
     _fields_ = [("type", ctypes.c_ulong),
                 ("ii", Input_I)]
 
-
-
-
+#------------------
+# functions
 
 def PressKey(hexKeyCode):
     extra = ctypes.c_ulong(0)
@@ -68,15 +69,17 @@ def mouse_move(pos):
     x = Input( ctypes.c_ulong(0), ii_ )
     ctypes.windll.user32.SendInput(1, ctypes.pointer(x), ctypes.sizeof(x))
 
+#documentation for coding functions:
 #https://gist.github.com/tracend/912308
 #https://docs.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-sendinput
 #https://docs.microsoft.com/en-us/windows/win32/api/winuser/ns-winuser-input
+#https://docs.microsoft.com/en-us/windows/win32/inputdev/virtual-key-codes
 
 keycode = {
-    'w' : 0x11,
-    'a' : 0x1E,
-    's' : 0x1F,
-    'd' : 0x20
+    1 : 0x11, # w    'forward'  
+    2 : 0x1E, # a    'left'     
+    3 : 0x1F, # s    'right'    
+    4 : 0x20  # d    'backwards'
 }
 
 
@@ -95,15 +98,17 @@ def walk(key1, key2=None, duration=1):
 
 
 
-def turn(deg):
+def look(x, y):
     mouse_move((1,0))
     mouse_move((-1,0))
-    mouse_move((int(2.72*deg),0))
+    k_x = 1 #yet to be adjusted
+    k_y = 1 #yet to be adjusted
+    mouse_move((int(k_x*x),int(k_y*y)))
 
     
 
 #=======================================================
-
+#          TEST ZONE
 
 
 '''
@@ -120,8 +125,8 @@ import numpy as np
 for n in range(5):
     sleep(.5)
     print("movement no. {0}".format(n))
-    dx = int(np.random.normal(0, std))
-    dy = int(np.random.normal(0, std))
+    dx = int(rnorm(0, std))
+    dy = int(rnorm(0, std))
     mouse_move((dx,dy))
 
 mouse_move((1,0))
@@ -139,9 +144,15 @@ for n in range(5):
 
 print("Get ready...")
 sleep(1)
-walk('w','d')
-turn(-90)
-turn(-90)
+walk(1, duration=2)
+sleep(.3)
+walk(3)
+#look(-90,0)
+#look(-90,0)
 sleep(1)
-turn(-90)
-turn(-90)
+
+
+#smooth view movement experiment
+for n in range(50):
+    look(-2,1)
+    sleep(.001)
